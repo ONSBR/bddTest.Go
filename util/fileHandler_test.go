30c79cc4d4bcc8d2cd07640ed6acaa48c1ad50ad
Clovis Chedid <coichedid@gmail.com>
2016-05-04 13:10:42 -0300

    Page Object Parser

---
+++ util/fileHandler_test.go
@@ -0,0 +1,12 @@
+package util_test
+
+import (
+	. "github.com/ONSBR/bddTest.Go/util"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("FileHandler", func() {
+
+})
